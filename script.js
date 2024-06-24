126ff55185bcb7a1bbbb40267d8e3fdc6f1f47d9
Abdalla Diaai <56742529+abdalla-diaai@users.noreply.github.com>
2024-06-24 13:09:40 +0100

    added new page to redirect.

--- script.js
+++ script.js
@@ -1,36 +1,47 @@
-function parseJwt (token) {
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
+function parseJwt(token) {
+  // function to handle token returned from google sign in
+  var base64Url = token.split(".")[1];
+  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  var jsonPayload = decodeURIComponent(
+    window
+      .atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
+  //   console.log(token);
+  //   console.log(jsonPayload);
 
-    return JSON.parse(jsonPayload);
-}
-
-function decodeJwtResponse(data){
-    console.log(parseJwt(data))
-    signIn(parseJwt(data))
+  return JSON.parse(jsonPayload);
 }
 
 function signIn(payload) {
-    // Example: Store user information in local storage
-    localStorage.setItem('user', JSON.stringify({
-        id: payload.sub,
-        fullName: payload.name,
-        givenName: payload.given_name,
-        familyName: payload.family_name,
-        imageUrl: payload.picture,
-        email: payload.email
-    }))
-    // Example: Update the UI to show the user is signed in
-    document.getElementById('user-info').innerHTML = `
-        <img src="${payload.picture}" alt="User Image" />
-        <h2>${payload.name}</h2>
-        <p>${payload.email}</p>
-    `
-};
-
-    
+  // store user information in local storage
+  localStorage.setItem(
+    "user",
+    JSON.stringify({
+      id: payload.sub,
+      fullName: payload.name,
+      givenName: payload.given_name,
+      familyName: payload.family_name,
+      imageUrl: payload.picture,
+      email: payload.email,
+    })
+  );
 
 
+  window.location.href = "home.html";
+   
+}
+
+function decodeJwtResponse(data) {
+  //   console.log(parseJwt(data));
+  signIn(parseJwt(data));
+}
+
+function handleCredentialResponse(response) {
+  //   console.log(response);
+  decodeJwtResponse(response.credential);
+}
